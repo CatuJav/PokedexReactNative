36ae591250a20f6e79b05365a74c38405ccb1a7e
Silvio <jalcoser1996@gmail.com>
2021-12-05 23:25:56 -0500

    Precarga de la información para buscar por el nombre

--- src/hooks/usePokemonSearch.tsx
+++ src/hooks/usePokemonSearch.tsx
@@ -2,40 +2,37 @@ import { useEffect, useRef, useState } from "react";
 import { pokemonApi } from '../api/pokemonApi';
 import { PokemonPaginatedResponse, Result, SimplePokemon } from '../interfaces/pokemonInterfaces';
 
-export const usePokemonPaginated = () => {
+export const usePokemonSearch = () => {
 
-    const [isLoading, setIsLoading] = useState(true);
+    const [isFetching, setIsFetching] = useState(true);
     
     const [simplePokemonList, setsimplePokemonList] = useState<SimplePokemon[]>([])
 
-    const nextPageUrl = useRef('https://pokeapi.co/api/v2/pokemon?limit=40')
+   
     const loadPokemos=async ()=>{
-        setIsLoading(true);
-        const resp = await pokemonApi.get<PokemonPaginatedResponse>(nextPageUrl.current);
-        nextPageUrl.current= resp.data.next;
+        const resp = await pokemonApi.get<PokemonPaginatedResponse>('https://pokeapi.co/api/v2/pokemon?limit=1200');
         mapPokemoList(resp.data.results)
         
     }
 
     const mapPokemoList = (pokemonList:Result[])=>{
-    
         const newPokemolist:SimplePokemon[]= pokemonList.map(({name,url})=>{
             const urlParts = url.split('/');
             const id = urlParts[urlParts.length-2];
             const picture = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`;
             return {id,picture,name,url}
         });
-        setsimplePokemonList([...simplePokemonList,...newPokemolist]);
-        setIsLoading(false);
+        setsimplePokemonList([...newPokemolist]);
+        setIsFetching(false);
     }
     useEffect(() => {
         loadPokemos();
     }, [])
 
     return{
-        isLoading,
+        isFetching,
         simplePokemonList,
-        loadPokemos
+        
     }
     
 }
--- src/screens/SearchScreen.tsx
+++ src/screens/SearchScreen.tsx
@@ -1,12 +1,26 @@
 import React from 'react'
-import { Platform, Text, View } from 'react-native'
+import { ActivityIndicator, FlatList, Platform, StyleSheet, Text, View } from 'react-native'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import { PokemonCard } from '../components/PokemonCard';
 import { SearchInput } from '../components/SearchInput';
-
+import { usePokemonSearch } from '../hooks/usePokemonSearch';
+import {styles as globalStyles} from '../theme/appTheme'
 export const SearchScreen = () => {
 
     const {top}=useSafeAreaInsets();
+    const {isFetching,simplePokemonList}=usePokemonSearch();
 
+    if (isFetching) {
+        return (
+        <View
+            style={{
+               ...styles.activityContainer
+            }}
+        >
+            <ActivityIndicator size={50} color='grey'/>
+            <Text>Cargando...</Text>
+        </View>)
+    }
     return (
         <View style={{
             flex:1, 
@@ -14,6 +28,43 @@ export const SearchScreen = () => {
             marginHorizontal:20
         }}>
             <SearchInput/>
+            <FlatList
+                    showsVerticalScrollIndicator={false}
+                    data={simplePokemonList}
+                    keyExtractor={(pokemon) => pokemon.id + pokemon.name}
+                    renderItem={({ item, index }) => (
+                        <PokemonCard
+                            pokemon={item}
+                        />
+                    )}
+                    //Numero de columnas en la flatlist
+                    numColumns={2}
+
+                    //Header
+                    ListHeaderComponent={
+                        (
+                            <Text style={{
+                                ...globalStyles.title,
+                                ...globalStyles.globalMargin,
+                                
+                                paddingBottom:10
+
+                            }}>
+                                Pókedex
+                            </Text>
+                        )
+                    }
+
+                />
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    activityContainer:{
+        flex:1,
+       // backgroundColor:'red',
+        justifyContent:'center',
+        alignItems:'center'
+    }
+})
